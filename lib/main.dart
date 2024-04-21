e63f1358a627966d6e01afef7f08802fe49708d8
mohdshahzil <theshahzil@gmail.com>
2024-04-21 12:17:12 +0530

    upgrade commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -8,26 +8,11 @@ final colorScheme = ColorScheme.fromSeed(
   background: const Color.fromARGB(255, 56, 49, 66),
 );
 
-final theme = ThemeData().copyWith(
-  useMaterial3: true,
-  scaffoldBackgroundColor: colorScheme.background,
-  colorScheme: colorScheme,
-  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-    titleSmall: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleMedium: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleLarge: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-);
+
 
 void main() {
   runApp(
-      const MyApp(),
+    const MyApp(),
   );
 }
 
@@ -37,9 +22,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: 'Great Places',
-      theme: theme,
-      home: ...,
+      theme: ThemeData(
+        useMaterial3: true,
+        textTheme: GoogleFonts.ubuntuCondensedTextTheme(),
+        colorScheme: colorScheme,
+        
+      ),
+      home: const PlacesScreen(),
     );
   }
 }
