e096479034df4efa44c4a29fabcaad717f8c1707
mohdshahzil <theshahzil@gmail.com>
2024-04-22 22:33:34 +0530

    add place screen added

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -9,8 +9,12 @@ class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return const Center(
-        child: Text('No Places added yet'),
+      return Center(
+        child: Text('No Places added yet',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground)),
       );
     }
     return ListView.builder(
